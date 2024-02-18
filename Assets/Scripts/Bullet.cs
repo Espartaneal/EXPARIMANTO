73a0d677f93cb379cb162d443f8bf6f97b8f8333
MHamadan-JY <159271149+MHamadan-JY@users.noreply.github.com>
2024-02-18 18:01:14 +0300

    Updated Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -13,11 +13,7 @@ public class Bullet : MonoBehaviour
             CreateBulletImpactEffect(collision);
             Destroy(gameObject);
         }
-        if (collision.gameObject.CompareTag("Zombie"))
-        {
-            collision.gameObject.GetComponent<Zombie1>().TakeDamage(bulletDamage);
-            Destroy(gameObject);
-        }
+        
     }
     void CreateBulletImpactEffect(Collision collision)
     {
